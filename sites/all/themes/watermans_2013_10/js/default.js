5693b4b162e987ac415dc3fdc428ccfc96e8b6ca
Callum Elliot <callum@sakurabrae.co.uk>
2013-11-12 12:18:06 +0000

    Om Sub menu added; Person page styled;

--- sites/all/themes/watermans_2013_10/js/default.js
+++ sites/all/themes/watermans_2013_10/js/default.js
@@ -5,7 +5,17 @@
       $('body', context).once('sitesetup', function () {
         $('body').bind('responsivelayout', function (e, d) {	
           // desktop version
+          // for all
+          if ($('#node_person_full_group_person_contact').length > 0){
+            $('#node_person_full_group_person_contact h3 span').append(' ').append($('h1#page-title').text());
+          }
           if (d.to == 'normal' ) {
+            // set up the main menu sub menu content positions
+            $('#om-menu-main-menu > li > .om-maximenu-content').each(function(){
+             var leftpos = $(this).parent().position();
+             //console.log(leftpos.left);
+             $(this).css({'left':'-' + leftpos.left +'px'});
+            });
             // move the side blocks to the outer wrapper
             $('.block-13, .block-14').appendTo('#zone-content-wrapper');
           }
---
+++ sites/all/themes/watermans_2013_10/templates/views-view-fields--person-contact--block.tpl.php
@@ -0,0 +1,40 @@
+<?php
+
+/**
+ * @file
+ * Default simple view template to all the fields as a row.
+ *
+ * - $view: The view in use.
+ * - $fields: an array of $field objects. Each one contains:
+ *   - $field->content: The output of the field.
+ *   - $field->raw: The raw data for the field, if it exists. This is NOT output safe.
+ *   - $field->class: The safe class id to use.
+ *   - $field->handler: The Views field handler object controlling this field. Do not use
+ *     var_export to dump this object, as it can't handle the recursion.
+ *   - $field->inline: Whether or not the field should be inline.
+ *   - $field->inline_html: either div or span based on the above flag.
+ *   - $field->wrapper_prefix: A complete wrapper containing the inline_html to use.
+ *   - $field->wrapper_suffix: The closing tag for the wrapper.
+ *   - $field->separator: an optional separator that may appear before a field.
+ *   - $field->label: The wrap label text to use.
+ *   - $field->label_html: The full HTML of the label to use including
+ *     configured element type.
+ * - $row: The raw result object from the query, with all data it fetched.
+ *
+ * @ingroup views_templates
+ */
+?>
+<?php 
+/**
+ * Person contact details for view: admin/structure/views/view/person_contact/edit/block
+ */
+$title = explode(' ',$fields['title']->raw);
+$email = strip_tags($fields['field_email']->content);
+?>
+<h2 class="block-title">Contact <?php echo $title[0]; ?></h2>
+<div class="button bluegrad gradient">
+<p><a href="mailto:<?php echo $email; ?>">Email <?php echo $title[0]; ?></a></p>
+</div>
+<div class="button redgrad gradient">
+<p><a href="/how-claim/start-your-claim-now">Ask A Question</a></p>
+</div>
