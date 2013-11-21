49d74706877a7dcf566cce94f946e93a2bbd2128
Callum Elliot <callum@sakurabrae.co.uk>
2013-11-21 14:57:46 +0000

    youtube updated - removed from media & added to ckeditor; image styles flush added; metatag added; people section compelted;

--- sites/all/themes/watermans_2013_10/js/default.js
+++ sites/all/themes/watermans_2013_10/js/default.js
@@ -8,14 +8,16 @@
           
           // FAQs
           if ($('.context-frequently-asked-questions').length > 0){
-              $('.view-faq .views-field-body').each(function(){$(this).hide();});  
-              $('.view-faq .views-field-title a').click(function(e){
-                e.preventDefault();
-                $('.faq-active').parent().parent().next().hide();
-                $('.faq-active').removeClass('faq-active');
-                $(this).parent().parent().next().show();
-                $(this).addClass('faq-active');
-              });      
+            $('.view-faq .views-field-body').each(function(){
+              $(this).hide();
+            });  
+            $('.view-faq .views-field-title a').click(function(e){
+              e.preventDefault();
+              $('.faq-active').parent().parent().next().hide();
+              $('.faq-active').removeClass('faq-active');
+              $(this).parent().parent().next().show();
+              $(this).addClass('faq-active');
+            });      
           }
           
           // add the category to the people search results
@@ -24,15 +26,45 @@
           }
           
           if (d.to == 'normal' ) {
+            // people section match the rhs col boxes to the height of the users
+            if ($('.view-people-listing').length > 0){
+              $('.region-sidebar-second .block-block-19,.region-sidebar-second .block-block-21,.region-sidebar-second .block-block-22').hide();
+              var liMarginBottom = parseInt($('.view-people-listing ul li').css('marginBottom'));
+              $('.view-people-listing .item-list').each(
+                function(){
+                  var section = $(this).find('h3').text();
+                  // -20 for the top & bottom padding in the block
+                  var sectionHeight = ($(this).height() - liMarginBottom) - 20;
+                  // console.log(sectionHeight);
+                  //console.log(section);
+                  switch (section) { 
+                    case 'Court Department':
+                      $('.region-sidebar-second .block-block-19').css({'height':sectionHeight+'px'}).show();
+                      break;
+                    case 'RTA Department':
+                      $('.region-sidebar-second .block-block-21').css({'height':sectionHeight+'px'}).show();
+                      break;
+                    case 'Business Development':
+                      $('.region-sidebar-second .block-block-22').css({'height':sectionHeight+'px'}).show();
+                      break;
+                  }
+                });
+            }
+            
             // move news thumnbail to first para of body copy
             if ($('.node-type-news-item').length > 0){
-              $('.field-name-field-image img').css({'float':'right','margin':'0 0 10px 10px'}).prependTo($('.field-name-body p:first'));
+              $('.field-name-field-image img').css({
+                'float':'right',
+                'margin':'0 0 10px 10px'
+              }).prependTo($('.field-name-body p:first'));
             }
             // set up the main menu sub menu content positions
             $('#om-menu-main-menu > li > .om-maximenu-content').each(function(){
-             var leftpos = $(this).parent().position();
-             //console.log(leftpos.left);
-             $(this).css({'left':'-' + leftpos.left +'px'});
+              var leftpos = $(this).parent().position();
+              //console.log(leftpos.left);
+              $(this).css({
+                'left':'-' + leftpos.left +'px'
+              });
             });
             // team page add padding to sidebar
             if ($('.block-people-listing-block').length){
@@ -40,7 +72,9 @@
               var sidebar = $('.region-sidebar-second').offset().top;
               var blockPadding = thisBlock - sidebar;
               if (blockPadding > 0){
-                $('.region-sidebar-second').css({'padding-top':blockPadding+'px'});
+                $('.region-sidebar-second').css({
+                  'padding-top':blockPadding+'px'
+                });
               }
             }
             // move the side blocks to the outer wrapper
