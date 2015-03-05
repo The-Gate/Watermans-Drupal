d949fc22b844617fd640e01c9c7548be91535e7d
Callum Elliot <callum@sakurabrae.co.uk>
2015-03-05 07:26:38 +0000

    drupal core & modules update;

---
+++ index-maintenance.php
@@ -0,0 +1,61 @@
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Strict//EN"
+  "http://www.w3.org/TR/xhtml1/DTD/xhtml1-strict.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml" xml:lang="en" lang="en" dir="ltr">
+
+<head>
+  <title>Site under maintenance | Watermans - Accident Claims and Care</title>
+  <meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+<link rel="shortcut icon" href="http://dev.watermans.ws/sites/default/files/favicon.ico" type="image/vnd.microsoft.icon" />
+  <link type="text/css" rel="stylesheet" href="http://dev.watermans.ws/sites/default/files/css/css_xE-rWrJf-fncB6ztZfd2huxqgxu4WO-qwma6Xer30m4.css" media="all" />
+<link type="text/css" rel="stylesheet" href="http://dev.watermans.ws/sites/default/files/css/css_k0wkk-WdSzbt1AOY5c01ax8M1d3hXOwqByY8zTUE8R0.css" media="all" />
+<link type="text/css" rel="stylesheet" href="http://dev.watermans.ws/sites/default/files/css/css_MnXiytJtb186Ydycnpwpw34cuUsHaKc80ey5LiQXhSY.css" media="all" />
+  <script type="text/javascript" src="http://dev.watermans.ws/misc/jquery.js?v=1.4.4"></script>
+<script type="text/javascript" src="http://dev.watermans.ws/misc/jquery.once.js?v=1.2"></script>
+<script type="text/javascript" src="http://dev.watermans.ws/misc/drupal.js?nkl23s"></script>
+<script type="text/javascript" src="http://dev.watermans.ws/misc/jquery.cookie.js?v=1.0"></script>
+<script type="text/javascript" src="http://dev.watermans.ws/sites/all/modules/om_maximenu/js/om_maximenu.js?nkl23s"></script>
+<script type="text/javascript" src="http://dev.watermans.ws/sites/all/modules/resp_img/resp_img.js?nkl23s"></script>
+<script type="text/javascript">
+<!--//--><![CDATA[//><!--
+jQuery.extend(Drupal.settings, {"basePath":"\/","pathPrefix":"","ajaxPageState":{"theme":"watermans_2013_10","theme_token":"cd8ZQyduzEaO4N7tob2N5Lyytq9BVx3JCTYn8MqSs48","css":{"modules\/system\/system.base.css":1,"modules\/system\/system.menus.css":1,"modules\/system\/system.messages.css":1,"modules\/system\/system.theme.css":1,"modules\/comment\/comment.css":1,"modules\/field\/theme\/field.css":1,"modules\/node\/node.css":1,"sites\/all\/modules\/om_maximenu\/css\/om_maximenu.css":1,"modules\/search\/search.css":1,"modules\/user\/user.css":1,"sites\/all\/modules\/views\/css\/views.css":1,"sites\/all\/modules\/ctools\/css\/ctools.css":1},"js":{"misc\/jquery.js":1,"misc\/jquery.once.js":1,"misc\/drupal.js":1,"misc\/jquery.cookie.js":1,"sites\/all\/modules\/om_maximenu\/js\/om_maximenu.js":1,"sites\/all\/modules\/resp_img\/resp_img.js":1}},"respImg":{"default_suffix":"__mobile","current_suffix":false,"forceRedirect":"0","forceResize":"1","reloadOnResize":"0","useDevicePixelRatio":0,"suffixes":{"__normal":481,"__mobile":1}}});
+//--><!]]>
+</script>
+</head>
+<body class="maintenance-page in-maintenance no-sidebars">
+  <div id="page">
+    <div id="header">
+      <div id="logo-title">
+
+        
+        <div id="name-and-slogan">
+          
+                  </div> <!-- /name-and-slogan -->
+      </div> <!-- /logo-title -->
+
+      
+    </div> <!-- /header -->
+
+    <div id="container" class="clearfix">
+
+      
+      <div id="main" class="column"><div id="main-squeeze">
+
+        <div id="content">
+          <h1 class="title" id="page-title">Site under maintenance</h1>                    <div id="content-content" class="clearfix">
+            Watermans is currently under maintenance. We should be back shortly. Thank you for your patience.          </div> <!-- /content-content -->
+        </div> <!-- /content -->
+
+      </div></div> <!-- /main-squeeze /main -->
+
+      
+    </div> <!-- /container -->
+
+    <div id="footer-wrapper">
+      <div id="footer">
+              </div> <!-- /footer -->
+    </div> <!-- /footer-wrapper -->
+
+  </div> <!-- /page -->
+
+</body>
+</html>
