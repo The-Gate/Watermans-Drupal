d9fcb4bc8b49ef91044c14eb67d8e88b00bd8884
Callum Elliot <callum@sakurabrae.co.uk>
2015-06-02 07:34:18 +0100

    forms updated to hubspot;

--- sites/all/themes/watermans_2013_10/templates/html.tpl.php
+++ sites/all/themes/watermans_2013_10/templates/html.tpl.php
@@ -148,6 +148,15 @@ return"";}};}();var removeServiceTickFlash=function(swfobject){try {var containe
       s.parentNode.insertBefore(adiSrc, s);
    })();
 </script>
-
+<!-- Start of Async HubSpot Analytics Code -->
+  <script type="text/javascript">
+    (function(d,s,i,r) {
+      if (d.getElementById(i)){return;}
+      var n=d.createElement(s),e=d.getElementsByTagName(s)[0];
+      n.id=i;n.src='//js.hs-analytics.net/analytics/'+(Math.ceil(new Date()/r)*r)+'/437852.js';
+      e.parentNode.insertBefore(n, e);
+    })(document,"script","hs-analytics",300000);
+  </script>
+<!-- End of Async HubSpot Analytics Code -->
 </body>
 </html>
