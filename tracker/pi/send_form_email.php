7d4d1006f6cebb751b8722510f877bcb0d6a0a71
Callum Elliot <callum@sakurabrae.co.uk>
2017-10-26 17:09:51 +0100

    hubspot removed from parts of site; modules updates required for media images to have corrrect links; custom html / js udated to send email via php mail();

--- tracker/pi/send_form_email.php
+++ tracker/pi/send_form_email.php
@@ -2,9 +2,10 @@
 
 if (isset($_POST['email'])) {
   // EDIT THE 2 LINES BELOW AS REQUIRED
-  $email_to = "firstresponse@watermans.ws";
+//  $email_to = "firstresponse@watermans.ws";
 //  $email_to = "callum@sakurabrae.co.uk";
-  $email_subject = "The following enquiry was made through the Personal Injury Calculator at Watermans.ws";
+  $email_to = "kate.brown@thegateworldwide.com";
+  $email_subject = "The following enquiry was made through the Personal Injury Calculator at watermans.co.uk";
 
   function died($error) {
     // your error code can go here
@@ -16,27 +17,31 @@ if (isset($_POST['email'])) {
   }
 
   // validation expected data exists
-  if (!isset($_POST['full_name']) ||
+  if (!isset($_POST['firstname']) ||
+      !isset($_POST['lastname']) ||
       !isset($_POST['email']) ||
+      !isset($_POST['phone']) ||
       !isset($_POST['Injury'])) {
-    died('We are sorry, but there appears to be a problem with the form you submitted.');
+    died('We are sorry, but there appears to be a problem with the form you submitted. Please make sure you have entered all the information correctly.');
   }
   else {
-    $first_name = $_POST['full_name']; // required
+    $fullname = $_POST['firstname'] . ' ' . $_POST['lastname']; // required
     $email_from = $_POST['email']; // required
     $injury = $_POST['Injury']; // required
+    $phone = $_POST['phone']; // required
     $error_message = "";
     $email_exp = '/^[A-Za-z0-9._%-]+@[A-Za-z0-9.-]+\.[A-Za-z]{2,4}$/';
     if (!preg_match($email_exp, $email_from)) {
       $error_message .= 'The Email Address you entered does not appear to be valid.<br />';
     }
-    $string_exp = "/^[A-Za-z .'-]+$/";
-    if (!preg_match($string_exp, $first_name)) {
-      $error_message .= 'The Name you entered does not appear to be valid.<br />';
-    }
     if (strlen($error_message) > 0) {
       died($error_message);
     }
+    $string_exp = "/^[A-Za-z .'-]+$/";
+    if (!preg_match($string_exp, $fullname)) {
+      $error_message .= 'The Name you entered does not appear to be valid.<br />';
+    }
+
     $email_message = "Form details below.\n\n";
 
     function clean_string($string) {
@@ -44,16 +49,17 @@ if (isset($_POST['email'])) {
       return str_replace($bad, "", $string);
     }
 
-    $email_message .= "Full Name: " . clean_string($first_name) . "\n";
+    $email_message .= "Name: " . clean_string($fullname) . "\n";
     $email_message .= "Email: " . clean_string($email_from) . "\n";
     $email_message .= "Injury: " . clean_string($injury) . "\n";
+    $email_message .= "Phone: " . clean_string($phone) . "\n";
 // create email headers
     $headers = 'From: ' . $email_from . "\r\n" .
         'Reply-To: ' . $email_from . "\r\n" .
         'X-Mailer: PHP/' . phpversion();
 
-    @mail($email_to, $email_subject, $email_message, $headers);
-    echo('<script type="text/javascript">top.window.location="/personal-injury-compensation-calculator/thank-you";</script>');
+    mail($email_to, $email_subject, $email_message, $headers);
+    echo('<script type="text/javascript">this.location="/tracker/pi/thankyou.html";</script>');
   }
 }
 ?>
